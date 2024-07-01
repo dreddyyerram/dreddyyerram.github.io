dd72efdcff700a2a20099408ed42feca8e8b1ee1
Dharanidhar Reddy Yerram <yerramdharanidhar@gmail.com>
2024-07-01 12:59:09 +0530

    Added Prjects and edited toggel

--- _scripts/main.js
+++ _scripts/main.js
@@ -6,10 +6,9 @@ $(function() {
   const toggle = document.getElementById('toggle');
   const input = document.getElementById('switch');
 
-  if (night) {
-    input.checked = true;
-    body.classList.add('night');
-  }
+  input.checked = true;
+  body.classList.add('night');
+
 
   toggle.addEventListener('click', function() {
     const isChecked = input.checked;
